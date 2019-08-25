e90e2372acfb735d1cd939f4a561e958eb6835c0
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-25 13:56:20 -0500

    mini search function

---
+++ script.js
@@ -0,0 +1,21 @@
+// most list of google search results
+const googleDatabase = [
+	'dogs.com',
+	'sandwichrecipes.com',
+	'animals.com',
+	'apples.com',
+	'catspeople.com',
+	'callofduty.com',
+	'catsrock.com',
+	'adoptsomecats.com',
+	'catsoverdogs.com'
+];
+// search algorithm cut to 3 results at most
+const googleSearch = searchInput => {
+	const matches = googleDatabase.filter(website => {
+		return website.includes(searchInput);
+	});
+	return matches.length > 3 ? matches.slice(0, 3) : matches;
+};
+
+console.log(googleSearch('cats'));
